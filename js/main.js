d589ad918dcdb8ae84fce04deebd7501abd605df
CapitanJek <azim.esenov.0@example.com>
2024-07-02 15:37:00 +0600

    add home work

---
+++ js/main.js
@@ -0,0 +1,66 @@
+// типы даных
+// string
+// number
+// boolean
+// under
+// null
+// bigint
+// object
+// symbol
+
+// const нельзя изменить
+// const str = 'hello' ;
+
+// let можно переназначить
+// let num = 1
+// num = 2;
+//
+// console.log(num * 'item')
+
+// сравнение
+// присваение =
+// нестрогое сравнение ==
+// строгое сравнение ===
+
+// Наименование
+
+// string
+// stringItem
+// string_item
+// название соответствовать логике
+
+// Получить 10 в консоле
+// console.log(10)
+
+
+
+let booleanValue = false
+
+// console.log(booleanValue, 'booleanValue')
+
+// !0 = true
+// 1 = true
+// 0 = false
+
+//
+// console.log(7 > 5)
+// console.log(7 > 8)
+//
+
+// Дано число. Проверьте, отрицательное оно или нет.
+// Выведите об этом информацию в консоль.
+
+// let number = -10;
+// console.log(number > 0)
+
+// Дана строка. Выведите в консоль длину этой строки.
+// length
+
+// let a = 'helldfasfaao'
+// let number = '12324';
+// console.log(number.length, 'number length')
+
+// Дано число. Проверьте, четное оно или нет.
+const num = 9;
+console.log(num % 2 === 0)
+
